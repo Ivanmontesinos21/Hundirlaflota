8833874e31fc8d82760e8b657de874d92434efb2
imontesinosm <ivamonmor2@alu.edu.gva.es>
2022-11-15 21:07:57 +0100

    a

---
+++ tablero.java
@@ -0,0 +1,15 @@
+public class tablero {
+    public static void main(String[] args) {
+         String[][] tablero=new String[10][10];
+rellenar(tablero);
+    }
+    public  static void  rellenar(String tablero[][]) {
+        for (int j = 0; j < tablero.length; j++) {
+            for (int i = 0; i < tablero.length; i++) {
+
+            }
+        }
+
+    }
+
+    }
