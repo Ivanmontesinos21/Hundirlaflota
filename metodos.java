05b5d7e91ef3eb83b6cd636c752e5d044419812f
imontesinosm <ivamonmor2@alu.edu.gva.es>
2022-11-16 21:19:45 +0100

    Hundir la flota

--- metodos.java
+++ metodos.java
@@ -1,8 +1,87 @@
 public class metodos {
     public static void main(String[] args) {
+        char[][] tablero = new char[10][10];
+        char[][] tablero2 = new char[10][10];
+        inicializarTablero(tablero, tablero2);
+
 
     }
-    public static void inicializarTablero(char[][] tablero){
+
+
+    public static void inicializarTablero(char[][] tablero, char[][] tablero2) {
+        //tablero1
+        char mar = '~';
+
+
+        for (int x = 0; x < tablero.length; x++) {
+
+            for (int y = 0; y < tablero.length; y++) {
+                tablero[x][y] = mar;
+
+
+            }
+
+
+        }
+
+
+        char letras = 'A';
+
+        for (int x = 0; x < tablero.length; x++) {
+            System.out.print(letras + " ");
+            for (int y = 0; y < tablero.length; y++) {
+
+
+                System.out.print(tablero[x][y] + " ");
+
+            }
+            System.out.println();
+
+            letras++;
+
+        }
+        System.out.print("");
+        for (int y = 0; y < tablero.length; y++) {
+            System.out.print(" " + y);
+        }
+        for (int x = 0; x < tablero2.length; x++) {
+            System.out.print(letras + " " + letras);
+            for (int y = 0; y < tablero2.length; y++) {
+
+
+                System.out.print(tablero2[x][y] + " ");
+        //hasta aqui tablero uno
+        //tablero 2
+
+
+
+
+            }
+
+
+        }
+
+
+        for (int x = 0; x < tablero2.length; x++) {
+            System.out.print(letras + " ");
+            for (int y = 0; y < tablero2.length; y++) {
+
+
+                System.out.print(tablero2[x][y] + " ");
+
+            }
+            System.out.println();
+
+            letras++;
+
+        }
+        System.out.print(" ");
+        for (int y = 0; y < tablero2.length; y++) {
+            System.out.print(" " + y);
+
+        }
 
     }
 }
+
+
--- tablero.java
+++ tablero.java
@@ -1,15 +1,64 @@
 public class tablero {
     public static void main(String[] args) {
-         String[][] tablero=new String[10][10];
-rellenar(tablero);
+        char[][] tableros = new char[10][10];
+        rellenar(tableros);
+        mostrar(tableros);
     }
-    public  static void  rellenar(String tablero[][]) {
-        for (int j = 0; j < tablero.length; j++) {
-            for (int i = 0; i < tablero.length; i++) {
+
+    private static void mostrar(char[][] tableros) {
+        //Tablero uno
+    char letras='A';
+     char numero='1';
+
+        for (int x = 0; x < tableros.length; x++) {
+
+            System.out.print(letras + " ");
+
+            for (int y = 0; y < tableros.length; y++) {
+
+
+
+                System.out.print( tableros[x][y] + " ");
+
 
             }
+
+            System.out.println();
+            letras++;
+            }
+        System.out.print("");
+        for (int y = 0; y < tableros.length; y++) {
+            System.out.print( " " + y );
+            //hasta aqui tablero uno
+
+        }
+
+        }
+
+
+
+
+    public static void rellenar(char tableros[][]) {
+
+        char mar = '~';
+
+        for (int x = 0; x < tableros.length; x++) {
+
+            for (int y = 0; y < tableros.length; y++) {
+                tableros[x][y] = mar;
+
+
+            }
+
+
+        }
+
+
+
         }
 
     }
 
-    }
+
+
+
